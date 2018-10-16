b46ff9683b2e6ddf33df368fe32d6b5a3f17e2d9
Jeremy Bean <j@jeremybean.com>
2018-10-16 12:06:26 -0400

    Cleanup of logs and backups

--- src/old/maxload.h
+++
@@ -1,6 +0,0 @@
-/* vals from db.c */
-#define MAXLOAD_KEY_HASH 127
-#define MAXLOADFILE "../area/maxload.txt"
-extern ITEM_MAX_LOAD    * maxload_index_hash      [MAXLOAD_KEY_HASH];
-extern int                top_maxload;
-       int                maxload_chain        args( (void) );
